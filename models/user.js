5e88cd27ace97ae3621f643859c68c7f6bf7cab3
Shubham-S-Nimje <shubhamsnimje@gmail.com>
2023-09-12 15:09:55 +0530

    Getting Orders Placed

--- models/user.js
+++ models/user.js
@@ -91,6 +91,48 @@ class User {
       );
   }
 
+  addOrder() {
+    const db = getDb();
+    return this.getCart()
+      .then((products) => {
+        const order = {
+          items: products,
+          user: { _id: new ObjectId(this._id), name: this.name },
+        };
+        return db.collection("orders").insertOne(order);
+      })
+      .then((result) => {
+        this.cart = { items: [] };
+        return db
+          .collection("users")
+          .updateOne(
+            { _id: new ObjectId(this._id) },
+            { $set: { cart: { items: [] } } }
+          );
+      });
+  }
+
+  getOrders() {
+    const db = getDb();
+    return db
+      .collection("orders")
+      .find({ "user._id": new ObjectId(this._id) })
+      .toArray();
+    // .then((products) => {
+    //   return products.map((p) => {
+    //     return {
+    //       ...p,
+    //       quantity: this.cart.items.find((i) => {
+    //         return i.productId.toString() === p._id.toString();
+    //       }).quantity,
+    //     };
+    //   });
+    // })
+    // .catch((err) => {
+    //   console.log(err);
+    // });
+  }
+
   static findById(userId) {
     const db = getDb();
     return db
